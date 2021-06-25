bf23563e7c68a6927eadff7576c13aa73bc17dcb
Alexandre Melo <xandymelo@outlook.com.br>
2021-06-25 00:30:46 -0300

    Create 1001.go

---
+++ 1001.go
@@ -0,0 +1,16 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+
+    var input1 int
+	var input2 int
+ 	fmt.Scan(&input1)
+    fmt.Scan(&input2)
+    X := input1 + input2
+    fmt.Printf("X = %d\n", X)
+
+}
